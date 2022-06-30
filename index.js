724bb0547f4cdf548d18ce830099629c97959c83
JuliaZel <91857302+JuliaZel@users.noreply.github.com>
2022-06-30 16:05:51 -0400

    Update about, theme, projects csv

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import ProjectPage from './components/ProjectPage.js';
 import Navbar from './components/Navbar.js';
 
 Promise.all([
-      d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRzuPHi-ZjhY9HfLv7HjMCCwy6NyzWJyxjj03fHfSJtTHAkCkvoN8Lm705rWP-y5LFMDmXj-9rBe_qC/pub?output=csv"),
-      d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSNhuYWG4ylr1-xZSDcBunRrKJyLqPIC37VOGoW8QG0Knve2YeIem2az2t0vVXz1769WALBxjs3U_J2/pub?output=csv"),
-      d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQFY7mscoie7ADrYeLpnUwHhA3wDZ9lF0Go-n-q_IT2m-2BrtcZ9bvAv8PSIsQBjAFy_4YypxLRmj9U/pub?output=csv"),
+      d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT-zPPHh9Ete3XXUORGYBNQrCNCH0ooozM1zM9mi2PB5bG3hcqXz9_rQx80z8tLFe33QVZPuELZWS9y/pub?output=csv"),
+      d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ6kGJZhyGiIsk-vzAj3gOOmdimxtoo0c5x4DGWb_1I1dZuuNxprORaPzaNShMKUikqjkegV6fm3r3k/pub?output=csv"),
+      d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vS4A7YN0Q9PftzDncQePwHI-XvH12oK5t3ckeRMj4K0nweJuXQ6TzMrwjNxotHwGKTtyDi_CjF6uZ-G/pub?output=csv"),
       ])
       .then(([about, themes, projects]) => {
         const data = {about, themes, projects};
